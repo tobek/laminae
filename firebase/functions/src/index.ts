e9b77f84ec69b82cf3dbd4e735368cbd498d186f
Toby Fox <tobyfox@gmail.com>
2021-06-02 15:54:24 -0700

    🌱

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -5,32 +5,53 @@ admin.initializeApp();
 
 // https://firebase.google.com/docs/functions/typescript
 
-export const boop = functions.https.onRequest((req, res) => {
+export const migrate = functions.https.onRequest(async (req, res) => {
   const db = admin.database();
+  // function increment(key: string) {
+  //   const ref = db.ref(key);
+  //   ref.transaction((count) => (count || 0) + 1);
+  // }
+  // increment("/testfoobarbaz");
+
+  const snapshot = await db.ref("/").get();
+  db.ref("/laminae").set(snapshot.toJSON());
+  db.ref("/beeps").remove();
+  db.ref("/beepsByPage").remove();
+  db.ref("/boops").remove();
+  db.ref("/boopsByPage").remove();
+
+  res.send(snapshot.toJSON());
+});
+
+export const getAll = functions.https.onRequest(async (req, res) => {
+  const db = admin.database();
+  const snapshot = await db.ref("/").get();
+  res.send(snapshot.toJSON());
+});
+
+export const boop = functions.https.onRequest((req, res) => {
+  const project = req.query.project;
   const pageId = req.query.id;
   const link = req.query.link;
 
+  const db = admin.database();
+  function increment(key: string) {
+    const ref = db.ref((project ? project + "/" : "") + key);
+    ref.transaction((count) => (count || 0) + 1);
+  }
+
   if (link) {
-    const beepsByPage = db.ref("/beepsByPage/" + pageId + "/" + link);
-    beepsByPage.transaction((current) => {
-      return (current || 0) + 1;
-    });
-    const beepsRef = db.ref("/beeps");
-    beepsRef.transaction((current) => {
-      return (current || 0) + 1;
-    });
+    increment("/beepsByPage/" + pageId + "/" + link);
+    increment("/beeps");
   } else {
     if (pageId) {
-      const boopsByPageRef = db.ref("/boopsByPage/" + pageId);
-      boopsByPageRef.transaction((current) => {
-        return (current || 0) + 1;
-      });
+      increment("/boopsByPage/" + pageId);
     }
+    increment("/boops");
 
-    const boopsRef = db.ref("/boops");
-    boopsRef.transaction((current) => {
-      return (current || 0) + 1;
-    });
+    const date = (new Date()).toISOString().substr(0, 10);
+    increment("/boopsByDay/" + date);
+    increment("/boopsByMonth/" + date.substr(0, 7));
   }
 
   res.send("ok");
